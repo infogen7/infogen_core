a4cfc8b353be044ec7b8ae85cf7baba248968d92
larry lv <larrylv@outlook.com>
2021-04-09 08:52:49 +0800

    更新版本

--- src/main/java/com/infogen/auth/Auth.java
+++ src/main/java/com/infogen/auth/Auth.java
@@ -4,7 +4,7 @@ import java.security.NoSuchAlgorithmException;
 import com.infogen.time.Time;
 
 public class Auth {
-	public static String HEAD_KEY_TOKEN = "X-DCToken";
+	public static String HEAD_KEY_TOKEN = "X-IGToken";
 
 	public static String sign(String appID, String appKey, String service, String method) throws NoSuchAlgorithmException {
 		Long timestamp = Time.GMT8();
